13c6cd5ce6008e547526b905b7b622f617519abe
dadabin0519 <dadabin0519@naver.com>
2024-08-05 10:55:00 +0900

    setup

---
+++ MMORPGOPENWORLD/Source/MMORPGOPENWORLD/MMORPGOPENWORLDGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MMORPGOPENWORLDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MMORPGOPENWORLD_API AMMORPGOPENWORLDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
